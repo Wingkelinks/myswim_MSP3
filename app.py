9c9aa00aa7403964f05877e8459c0ede9be96f84
Whitney Turner <whitters.t@gmail.com>
2021-07-03 14:32:44 +1000

    Add Register template & Footer template

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template, 
     redirect, request,  session, url_for)
 from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
@@ -22,6 +23,11 @@ mongo = PyMongo(app) ## CREATE INSTANCE OF PYMONGO AND ADD FLASK APP OBJECT
 def get_sets():
     sets = mongo.db.sets.find()
     return render_template("sets.html", sets=sets)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
