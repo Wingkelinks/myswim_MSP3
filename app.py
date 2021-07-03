e75744af9546ca528874b9599a4fd7f58442e347
Whitney Turner <whitters.t@gmail.com>
2021-07-03 16:34:37 +1000

    Add Log In functionality

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import (
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
+from werkzeug.utils import escape
 if os.path.exists("env.py"):
     import env
 
@@ -47,6 +48,32 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Success! Welcome to MySwim.")
     return render_template("register.html")
+
+
+# LOG IN
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #check if username exists in db
+        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+
+        if existing_user:
+            #check hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    # put user into 'session' cookie
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome to MySwim {}!".format(request.form.get("username")))
+            else:
+                #invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
